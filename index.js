26b04632d55c8c825cdd68e85e721e2aa0d07582
Reshma <reshmaretnakumar22@gmail.com>
2019-04-04 22:31:00 +0530

    added validations

--- index.js
+++ index.js
@@ -47,10 +47,76 @@ app.post('/auth', function(request, response) {
         var records = [
             [username, password ,email,token]
           ];
+          
           connection.query("INSERT INTO accounts (username,password,email,token) VALUES ?", [records], function (err, result, fields) {
             if (err) throw err;
             console.log("Number of records inserted: " + result.affectedRows);
-            
+            userJSON["status"] = 'true';
+            userJSON["message"]= "Registration completed successfully";
+            userCredentials['token'] = token;
+            userJSON['data'] = userCredentials;
+            response.send(userJSON);
+            response.end();
+		});
+	} else {
+		response.send('Please enter valid Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/', function(request, response) {
+    const username = request.body.username;
+    const password = request.body.password;
+    let userJSON = {};
+    const currentTime = new Date();
+    console.log(currentTime);
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            console.log(results);
+            console.log("ttttttt",fields);
+			if (results.length > 0) {
+				request.session.loggedin = true;
+                request.session.username = username;
+                let userCredentials = {
+                    'userName' : results[0]['username'],
+                    'email':results[0]['email']
+                }
+                console.log(userCredentials);
+                //Create token
+                var token = jwt.sign(userCredentials, config.secret, {
+                    expiresIn: '2h' // expires in 2 hours
+                  }); 
+                var decoded = jwt.verify(token,config.secret,function(error,decode){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        console.log(decode);
+                    }
+                })
+                console.log(decoded);
+                request.session.token = token;
+                userJSON['status'] = true;
+                userJSON['message'] = "Registration Completed Sucessfully";
+                userJSON['data'] = {};
+                userJSON['data']['fullName'] = results[0]['username'];
+                userJSON['data']['password'] = results[0]['email'];
+                userJSON['data']['token'] = token;
+                response.send(userJSON);
+			} else {
+                let sampleJSON = {
+                    status:true,
+                    message:'Registration completed sucessfully',
+                    data:{
+                    fullname:'hello',
+                    email:'hello@me.com',
+                    profile_picture_url:'uploads/profile.png', //if profile pic is not present this field will be null,
+                    token:"AMnnk#$0278788498HAjaa"
+                    }
+                    };
+                // response.send('Incorrect Username and/or Password!');
+                response.send(sampleJSON);
+			}			
+			response.end();
 		});
 	} else {
 		response.send('Please enter Username and Password!');
